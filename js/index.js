5ae917b16ae5f27790eda5541b045bb1ae54dcfa
Tatiane <thathy23@gmail.com>
2024-03-19 15:34:14 -0300

    teste commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// a function nao está funcionando, o alert está seguindo
+//o comando contido no index.html
+
+        function alertar(){
+            alert ('PROJETO HTML PRONTO TESTAR')
+        }
+
+
+
+
+
+
+
